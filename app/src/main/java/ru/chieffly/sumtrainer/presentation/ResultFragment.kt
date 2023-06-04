20b2e7b2230d0c0e3eedfc307b3636af020d2094
Anton Bryantsev <chieffly@ya.ru>
2023-06-04 17:16:28 +0300

    game result screen

--- app/src/main/java/ru/chieffly/sumtrainer/presentation/ResultFragment.kt
+++ app/src/main/java/ru/chieffly/sumtrainer/presentation/ResultFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.FragmentManager
+import ru.chieffly.sumtrainer.R
 import ru.chieffly.sumtrainer.databinding.FragmentResultBinding
 import ru.chieffly.sumtrainer.domain.model.GameResult
 
@@ -35,13 +36,51 @@ class ResultFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        bindViews()
+        setClickListeners()
+    }
+
+    private fun setClickListeners() {
+        binding.btnUnderstand.setOnClickListener {
+            retryGame()
+        }
+
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 retryGame()
             }
         })
-
     }
+
+    private fun bindViews() {
+        with(binding) {
+            tvScore.text = String.format(
+                getString(R.string.result_score),
+                gameResult.countOfRightAnswers.toString()
+            )
+            tvScoreReq.text = String.format(
+                getString(R.string.require_score),
+                gameResult.gameSettings.minCountOfRightAnswers.toString()
+            )
+            tvPercent.text = String.format(
+                getString(R.string.result_percent),
+                getPercentOfRightAnswers().toString()
+            )
+            tvPercentReq.text = String.format(
+                getString(R.string.require_percent),
+                gameResult.gameSettings.minPercentOfRightAnswers.toString()
+            )
+        }
+    }
+
+    private fun getPercentOfRightAnswers() =
+        with(gameResult) {
+            if (countOfQuestions == 0) {
+                0
+            } else {
+                ((gameResult.countOfRightAnswers / gameResult.countOfQuestions.toDouble()) * 100).toInt()
+            }
+        }
 
     override fun onDestroyView() {
         super.onDestroyView()
